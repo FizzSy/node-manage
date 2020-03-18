cd295c83cb9ff33cb598d546472fc71c40c09c35
yuwen <1369530215@qq.com>
2020-03-18 15:15:01 +0800

    Revert "commit"
    This reverts commit c058b086335b09e9d9996d634c540bb7c74303c7.

--- serve.js
+++ serve.js
@@ -23,7 +23,7 @@ mongoose
 app.use(passport.initialize());
 // passportConfig(passport);
 passportConfig(passport);
-app.use("/manage", routes);
+app.use("/", routes);
 
 const port = process.env.PORT || 1002;
 
